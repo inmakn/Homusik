0558cfc1d877a0eee8279a8c1fadcae54dc165d3
inmakn <themousepolice@gmail.com>
2014-07-15 18:32:31 -0400

    signup now logs in new user automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:current_user] = user.id
       redirect_to(login_path)
     else
       render :new
